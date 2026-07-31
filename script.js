e2a4cc6b301ba45782ab4b1673eff6e7dba9b346
HeirenStuff <DNDHeiren@gmail.com>
2026-07-31 07:08:36 -0300

    nuke3

--- script.js
+++
@@ -1,6 +0,0 @@
-OBR.onReady(async () => {
-    const name = await OBR.player.getName();
-    const role = await OBR.player.getRole();
-    
-    document.getElementById("status").innerText = `Hola ${name}, eres ${role}`;
-});
